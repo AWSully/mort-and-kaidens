3245df898030b903a47825a87cff8b9e721f15a7
Austin Sullivan <AWSully@gmail.com>
2024-05-28 14:04:48 -0400

    removing some fluff

--- script.js
+++ script.js
@@ -32,43 +32,6 @@ toggleThemeBtn.addEventListener('click', () => {
 });
 
 // ===================================
-// STORIES SCROLL BUTTONS
-// Scrolling stories content
-storiesLeftButton.addEventListener('click', () => {
-  storiesContent.scrollLeft -= 320;
-});
-storiesRightButton.addEventListener('click', () => {
-  storiesContent.scrollLeft += 320;
-});
-
-// Checking if screen has minimun size of 1024px
-if (window.matchMedia('(min-width: 1024px)').matches) {
-  // Observer to hide buttons when necessary
-  const storiesObserver = new IntersectionObserver(
-    function (entries) {
-      entries.forEach((entry) => {
-        if (entry.target === document.querySelector('.story:first-child')) {
-          storiesLeftButton.style.display = entry.isIntersecting
-            ? 'none'
-            : 'unset';
-        } else if (
-          entry.target === document.querySelector('.story:last-child')
-        ) {
-          storiesRightButton.style.display = entry.isIntersecting
-            ? 'none'
-            : 'unset';
-        }
-      });
-    },
-    { root: storiesContent, threshold: 1 }
-  );
-
-  // Calling the observer with the first and last stories
-  storiesObserver.observe(document.querySelector('.story:first-child'));
-  storiesObserver.observe(document.querySelector('.story:last-child'));
-}
-
-// ===================================
 // POST MULTIPLE MEDIAS
 // Creating scroll buttons and indicators when post has more than one media
 posts.forEach((post) => {
